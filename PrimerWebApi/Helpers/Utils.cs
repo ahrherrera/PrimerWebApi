3792ddc7e21eba3cc1086d65d3da4b1047ac8a2a
Allan Ramirez <allanhavidramirez@gmail.com>
2021-11-12 21:09:29 -0600

    Initial commit

---
+++ PrimerWebApi/Helpers/Utils.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerWebApi.Helpers
+{
+    public class Utils
+    {
+
+        public static IConfigurationRoot GetConfiguration(IWebHostEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            return builder.Build();
+        }
+    }
+}
---
+++ PrimerWebApi/Startup.cs
@@ -0,0 +1,84 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using PrimerWebApi.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerWebApi
+{
+    public class Startup
+    {
+        private IWebHostEnvironment CurrentEnvironment { get; set; }
+        private IConfigurationRoot Configuration;
+
+        public Startup(IWebHostEnvironment env)
+        {
+            Configuration = Utils.GetConfiguration(env);
+            CurrentEnvironment = env;
+
+            //TODO: Add Environment Info
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllOrigins",
+                    builder =>
+                    {
+                        builder.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
+                    });
+            });
+
+            // Add Dependency Injection
+
+            var settingsSection = Configuration.GetSection("AppSettings");
+
+            services.AddResponseCompression();
+            services.AddMemoryCache();
+
+            //TODO: Configuración Final
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+
+            app.UseCors("AllOrigins");
+
+            //loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            //loggerFactory.AddDebug();
+
+            app.UseResponseCompression();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseStatusCodePages();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
